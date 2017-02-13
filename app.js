b44aede20cb982b284a9b6e12136cfa5be47c517
edplo <edplatomail@gmail.com>
2017-02-12 23:07:53 -0500

    massive changes to basic functionality

--- app.js
+++ app.js
@@ -1,77 +1,64 @@
 //60,000 divided by 120BPM = 500 milliseconds (.5 seconds)
 //multiply 1.5 for dotted values
 //multiply .667 for triplets
+var num = "";
+var multiplierArr = [];
 
-// var nameValue = document.getElementById("uniqueID").value;
-
-// var bpm = document.getElementById("form-bpm").value;
-
-// if(nameValue > 100){
-// 	console.log("bigger than 100");
-// }
-
-// $("form").serializeArray();
-
-// function submitBpm() {
-// 	// var bpm = document.getElementById("form-bpm").value;
-// 	// calculateBpmNotes(bpm);
-// 	// calculateBpmTriplets(bpm);
-// 	// autoBpm(bpm);
-// 	console.log("hi");
-
-// }
+function submitBpm() {
+	var bpm = document.getElementById("form-bpm").value;
+	bpmCheck(bpm);
+	multiplier(bpm);
+	notesCalc(bpm);
+	tripletsCalc(bpm);
+	dottedCalc(bpm);
+}
 
 
-
-// function calculateBpmNotes(bpm) {
-// 	document.getElementById("table-1").innerHTML = ((60000 / bpm)*4);
-// 	document.getElementById("table-2").innerHTML = (60000 / bpm)*2;
-// 	document.getElementById("table-4").innerHTML = (60000 / bpm);
-// 	document.getElementById("table-8").innerHTML = (60000 / bpm)/2;
-// 	document.getElementById("table-16").innerHTML = (60000 / bpm)/4;
-// 	document.getElementById("table-32").innerHTML = (60000 / bpm)/8
-// 	document.getElementById("table-64").innerHTML = (60000 / bpm)/16;
-// 	document.getElementById("table-128").innerHTML = (60000 / bpm)/32;
-// }
-
-// function calculateBpmTriplets(bpm) {
-// 	document.getElementById("table-1t").innerHTML = ((40000 / bpm)*4).toFixed(2);
-// 	document.getElementById("table-2t").innerHTML = (40000 / bpm)*2;
-// 	document.getElementById("table-4t").innerHTML = (40000 / bpm);
-// 	document.getElementById("table-8t").innerHTML = (40000 / bpm)/2;
-// 	document.getElementById("table-16t").innerHTML = (40000 / bpm)/4;
-// 	document.getElementById("table-32t").innerHTML = (40000 / bpm)/8
-// 	document.getElementById("table-64t").innerHTML = (40000 / bpm)/16;
-// 	document.getElementById("table-128t").innerHTML = (40000 / bpm)/32;
-// }
-
-// function calculateBpmDotted(bpm) {
-// 	document.getElementById("table-1d").innerHTML = (90000 / bpm)*4;
-// 	document.getElementById("table-2d").innerHTML = (90000 / bpm)*2;
-// 	document.getElementById("table-4d").innerHTML = (90000 / bpm);
-// 	document.getElementById("table-8d").innerHTML = (90000 / bpm)/2;
-// 	document.getElementById("table-16d").innerHTML = (90000 / bpm)/4;
-// 	document.getElementById("table-32d").innerHTML = (90000 / bpm)/8
-// 	document.getElementById("table-64d").innerHTML = (90000 / bpm)/16;
-// 	document.getElementById("table-128d").innerHTML = (90000 / bpm)/32;
-// }
+//safety reload if 0 or NaN entered
+function bpmCheck(bpm){
+	if(isNaN(bpm) || bpm == "" || bpm == 0) {
+		location.reload();
+	}
+}
 
 
-// function autoBpm(bpm){
-// var x = "";
-// var result = "";
-// for(var i=1; i<=8; i++){
-// 	x = "table-id" + i;
+//array for bpm multipliers
+function multiplier(num){
+	multiplierArr = [];
+	for(var y=.25; y<=32; y*=2){
+		multiplierArr.push(y);
+	}
+}
 
+//calculate notes table
+function notesCalc(bpm){
+var x = "";
+var y = "";
+for(var i=1; i<=8; i++){
+	x = "table-n" + i;
+	y = (60000 / bpm) / multiplierArr[i-1];
+	document.getElementById(x).innerHTML = y.toFixed(2);
+   }
+}
 
-// 	console.log(x);
-// 	}
+//calculate triplets table
+function tripletsCalc(bpm){
+var x = "";
+var y = "";
+for(var i=1; i<=8; i++){
+	x = "table-t" + i;
+	y = (40000 / bpm) / multiplierArr[i-1];
+	document.getElementById(x).innerHTML = y.toFixed(2);
+   }
+}
 
-
-// 	for(var y=.25; y<=32; y*=2){
-// 		console.log("y is" + y);
-
-// 	}
-// 	console.log("outside y is" + y);
-// 	document.getElementById(x).innerHTML = i;
-// }
+//calculate dotted table
+function dottedCalc(bpm){
+var x = "";
+var y = "";
+for(var i=1; i<=8; i++){
+	x = "table-d" + i;
+	y = (90000 / bpm) / multiplierArr[i-1];
+	document.getElementById(x).innerHTML = y.toFixed(2);
+   }
+}
